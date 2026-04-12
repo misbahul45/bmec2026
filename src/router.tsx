f9c7ea5f6d713b6f0f0d6540363445ab6cb4ebd6
taqin <misbahulmuttaqin395@gmail.com>
2026-04-12 15:59:02 +0700

    setup husky

--- src/router.tsx
+++ src/router.tsx
@@ -10,7 +10,7 @@ export function getRouter() {
 
   const router = createRouter({
     routeTree,
-    context: { queryClient },
+    context: { queryClient, user:null },
     defaultPreload: 'intent',
     defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
